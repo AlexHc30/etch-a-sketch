a0291faa732c5cb16b2a6e533f706c5340c43a34
Alex Haiduc <haiduc.alx@gmail.com>
2022-10-04 13:06:24 +0300

    Create function to generate board Function generates 16*16 divs created dinamically Divs are grouped into rows

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-console.log('hi there')
+let gridSize = 16;
+
+function generateBoard() {
+    const drawingBoard = document.querySelector('#drawingBoard');
+    for(let i = 1; i <= gridSize; i++) {
+        const row = document.createElement('div');
+        for(let j = 1; j <= gridSize; j++) {
+            const element = document.createElement('div');
+            element.classList = ('element');
+            row.appendChild(element);
+        }
+        row.classList = ('row');
+        drawingBoard.appendChild(row);
+    }
+}
+
+generateBoard();
