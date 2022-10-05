cc5f7ac9b1354cd6d815602b8d37ad7348d4273e
Alex Haiduc <haiduc.alx@gmail.com>
2022-10-05 15:32:25 +0300

    Change grid size to a custom one Set click listener for 'Custom grid size' button Create function so that user can change the drawing board size by typing in a value between 16 and 100 in the prompt. Remove previous grid Set condition so that the value has to be within the range of the specified value

--- index.js
+++ index.js
@@ -1,6 +1,4 @@
-let gridSize = 16;
-
-function generateBoard() {
+function generateBoard(gridSize) {
     const drawingBoard = document.querySelector('#drawingBoard');
     for(let i = 1; i <= gridSize; i++) {
         const row = document.createElement('div');
@@ -14,13 +12,33 @@ function generateBoard() {
     }
     const divs = document.querySelectorAll('.element');
     divs.forEach(div => {
-        div.addEventListener('mouseenter', drawing)
+        div.addEventListener('mouseenter', drawing);
         return div;
     });
 }
 
-generateBoard();
+generateBoard(16);
 
 function drawing(div) {
-    div.target.style = 'background-color : #999'
+    div.target.style = 'background-color : #999';
+}
+
+const customGrid = document.querySelector('#customGrid');
+customGrid.addEventListener('click', changeGridSize);
+
+function changeGridSize() {
+    const drawingBoard = document.querySelector('#drawingBoard');
+    while(drawingBoard.firstChild) {
+        drawingBoard.removeChild(drawingBoard.firstChild);
+    }
+    let customSize = prompt('Please choose a number between 16 and 100 for your drawing board size.');
+    console.log(customSize)
+    if(customSize >= 16 && customSize <= 100) {
+        generateBoard(customSize); 
+    } else if(!customSize) {
+        generateBoard(16);
+    } else {
+        customSize = prompt('Make sure you choose a number between 16 and 100 for your drawing board size');
+    }
+    
 }
