3f1427cd90609955237eba72e8753a0d401648a8
Alex Haiduc <haiduc.alx@gmail.com>
2022-10-10 15:21:44 +0300

    Add functionality to color squares to random RGB value Add 'Random Color' button to html Create event listener for button that captures click in variable Write function to generate random RGB values Modify drawing function so that if the button is clicked, square will be coloured randomly. Add condition to changeGridSize() so that if random color is clicked, all squares will reset to white. Made some minor css modifications to buttons appearance and alignment.

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+const defaultColor = '#333';
+let currentColor;
+
 function generateBoard(gridSize) {
     const drawingBoard = document.querySelector('#drawingBoard');
     for(let i = 1; i <= gridSize; i++) {
@@ -13,20 +16,32 @@ function generateBoard(gridSize) {
     const divs = document.querySelectorAll('.element');
     divs.forEach(div => {
         div.addEventListener('mouseenter', drawing);
-        return div;
     });
 }
-
 generateBoard(16);
 
+
+let randClicked;
+const randomColor = document.querySelector('#randomColor');
+    randomColor.addEventListener('click', () => {
+        return randClicked = true;
+    });
+    
 function drawing(div) {
-    div.target.style = 'background-color : #999';
+    if(randClicked) {
+        currentColor = generateRandomColor();
+        div.target.style = `background-color : ${currentColor}`;
+    } else if(!randClicked) {
+        currentColor = defaultColor;
+        div.target.style = `background-color : ${currentColor}`;
+    }
 }
 
 const customGrid = document.querySelector('#customGrid');
 customGrid.addEventListener('click', changeGridSize);
 
 function changeGridSize() {
+    randClicked = false;
     const drawingBoard = document.querySelector('#drawingBoard');
     while(drawingBoard.firstChild) {
         drawingBoard.removeChild(drawingBoard.firstChild);
@@ -40,5 +55,8 @@ function changeGridSize() {
     } else {
         customSize = prompt('Make sure you choose a number between 16 and 100 for your drawing board size');
     }
-    
+}
+
+const generateRandomColor = function() {
+    return 'hsl(' + Math.floor((Math.random() * 360)) + ', 80%, 60%)';
 }
