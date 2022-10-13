0ea3af09675aef9c977c862a5da25bdfe08ed704
Alex Haiduc <haiduc.alx@gmail.com>
2022-10-13 16:12:48 +0300

    Implement multiple features Add color pick input and button so that user can draw using custom color. Add eraser option and button. Add clear option which turns drawing board to default color. Add slider to change drawing board size and replace prompt method. Implement 'selected' class on currently selected button.

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const defaultColor = '#333';
+const defaultColor = '#ffffff00';
+const black = '#333';
 let currentColor;
 
 function generateBoard(gridSize) {
@@ -20,52 +21,99 @@ function generateBoard(gridSize) {
 }
 generateBoard(16);
 
+let colorClicked;
+const colorPick = document.querySelector('#colorPick');
+const colorButton = document.querySelector('#colorButton');
+colorButton.addEventListener('click', () => {
+    turnButtonFalse();
+    colorClicked = true;
+    colorButton.classList.add('selected');
+    return colorClicked;
+})
 
 let randClicked;
 const randomColor = document.querySelector('#randomColor');
 randomColor.addEventListener('click', () => {
-    return randClicked = true;
+    turnButtonFalse();
+    randClicked = true;
+    randomColor.classList.add('selected');
+    return randClicked;
 });
 
-const black = document.querySelector('#black');
-black.addEventListener('click', () => {
-    return randClicked = false;
+let blackClicked = true;
+const blackButton = document.querySelector('#black');
+blackButton.addEventListener('click', () => {
+    turnButtonFalse();
+    blackClicked = true;
+    blackButton.classList.add('selected');
+    return blackClicked;
 });
 
-    
+let eraseClicked;
+const eraser = document.querySelector('#eraser');
+eraser.addEventListener('click', () => {
+    turnButtonFalse();
+    eraseClicked = true;
+    eraser.classList.add('selected');
+    return eraseClicked;
+})
+
+function turnButtonFalse() {
+    colorClicked = randClicked = blackClicked = eraseClicked = false;
+    const buttons = document.querySelectorAll('button');
+    buttons.forEach(button => {
+        button.classList.remove('selected');
+    })
+    return [colorClicked,randClicked,blackClicked,eraseClicked];
+}
+
 function drawing(event) {
     if(event.buttons === 0) {
         return;
     } else {
-        if(randClicked) {
+        if (colorClicked) {
+            currentColor = colorPick.value;
+            event.target.style = `background-color : ${currentColor}`;
+        } else if(randClicked) {
             currentColor = generateRandomColor();
             event.target.style = `background-color : ${currentColor}`;
-        } else if(!randClicked) {
+        } else if(blackClicked) {
+            currentColor = '#333333';
+            event.target.style = `background-color : ${currentColor}`;
+        } else if(eraseClicked) {
             currentColor = defaultColor;
             event.target.style = `background-color : ${currentColor}`;
         }
     }
 }
 
-const customGrid = document.querySelector('#customGrid');
-customGrid.addEventListener('click', changeGridSize);
 
-function changeGridSize() {
+const generateRandomColor = function() {
+    return 'hsl(' + Math.floor((Math.random() * 360)) + ', 80%, 60%)';
+}
+
+
+const clear = document.querySelector('#clear').addEventListener('click', clearBoard);
+
+function clearBoard() {
+    const elements = document.querySelectorAll('.element');
+    elements.forEach(element => {
+        element.style.backgroundColor = defaultColor;
+    })
+}
+
+
+const slider = document.querySelector('#slider');
+let sizeValue = document.querySelector('#gridValue');
+let gridValue = slider.value;
+sizeValue.innerText = `${gridValue} x ${gridValue}`;
+
+slider.oninput = function changeGridValue() {
+    gridValue = slider.value;
+    sizeValue.innerText = `${gridValue} x ${gridValue}`;
     const drawingBoard = document.querySelector('#drawingBoard');
     while(drawingBoard.firstChild) {
         drawingBoard.removeChild(drawingBoard.firstChild);
     }
-    let customSize = prompt('Please choose a number between 16 and 100 for your drawing board size.');
-    console.log(customSize)
-    if(customSize >= 16 && customSize <= 100) {
-        generateBoard(customSize); 
-    } else if(!customSize) {
-        generateBoard(16);
-    } else {
-        customSize = prompt('Make sure you choose a number between 16 and 100 for your drawing board size');
-    }
-}
-
-const generateRandomColor = function() {
-    return 'hsl(' + Math.floor((Math.random() * 360)) + ', 80%, 60%)';
+    generateBoard(gridValue);
 }
